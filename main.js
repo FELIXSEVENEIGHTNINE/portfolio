e5e56747171d701e84dabe26a11dc6ee72f1d939
FELIXSEVENEIGHTNINE <felix567felix567@gmail.com>
2025-07-05 08:00:54 +0800

    1.0
    complete overhaul

---
+++ main.js
@@ -0,0 +1,43 @@
+// function removeNavClass(items) {
+//     if(items.length) 
+//     {
+//         items[0].className = '';
+//     }
+// }
+
+// function moveToHome(a)
+// {
+//     items = document.querySelectorAll('.active-link');
+//     removeNavClass(items);
+
+//     a.className = 'active-link';
+//     document.getElementById('web-home').className = 'active';
+
+//     document.getElementById('web-projects').className = '';
+//     document.getElementById('web-about').className = '';
+// }
+
+// function moveToProjects(a)
+// {
+//     items = document.querySelectorAll('.active-link');
+//     removeNavClass(items);
+
+//     a.className = 'active-link';
+//     document.getElementById('web-projects').className = 'active';
+
+//     document.getElementById('web-home').className = '';
+//     document.getElementById('web-about').className = '';
+// }
+
+// function moveToAbout(a)
+// {
+//     items = document.querySelectorAll('.active-link');
+//     removeNavClass(items);
+
+//     a.className = 'active-link';
+//     document.getElementById('web-about').className = 'active';
+
+//     document.getElementById('web-home').className = '';
+//     document.getElementById('web-projects').className = '';
+
+// }
